7374859d4a1906b4dc2d606ac1db97d8a83baa31
YUSYKOUD <pauloignaciofuentealba@gmail.com>
2026-08-16 17:24:34 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 window.CONVOKI_CONFIG = {
   // Completa estos 3 valores antes de publicar.
-  SUPABASE_URL: "",
-  SUPABASE_ANON_KEY: "",
+  SUPABASE_URL: "https://vgcaepoyknydaewxxjit.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_WoiT7Kxw8ATh24jIryjq-g_HdmQXHZc",
   // Ejemplo: "https://convoki.vercel.app"
   PUBLIC_BASE_URL: "https://convoki.vercel.app"
 };
